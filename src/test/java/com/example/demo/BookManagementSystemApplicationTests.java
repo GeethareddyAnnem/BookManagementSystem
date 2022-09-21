17b962d7ceb951dcc5d9112eec15a5bc34f8184c
Ashit Kumar Rai <68168801+ashitkumarrai@users.noreply.github.com>
2022-09-21 15:58:45 +0530

    hii

--- src/test/java/com/example/demo/BookManagementSystemApplicationTests.java
+++ src/test/java/com/example/demo/BookManagementSystemApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.entity.Book;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class BookManagementSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	public void selectBook() {
+		RestTemplate temp = new RestTemplate();
+		List<Book> books = (List<Book>) temp.getForObject("http://localhost:8080/Book",List.class);
+		System.out.println("book"+ books);
+		assertNotNull(books);
+		
+	}
+	
+	
 
 }
